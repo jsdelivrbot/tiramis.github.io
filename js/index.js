0919f2c5f2bac713fd51a5a6c1f7ab996b3e3fcc
tiramis <tiramissntreev@gmail.com>
2017-03-22 15:04:47 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,49 @@
-// ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
+// ID of the Google Spreadsheet
+ var spreadsheetID = "1ltYLzmbRbMnJ-2m8e-gt4b2cSjHPm0aB9jUsI33AQb4";
+ 
+ // Make sure it is public or set to Anyone with link can view 
+ var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+ 
+// make JSON call to Google Data API
+$.getJSON(url, function(data) {
+
+  // set global html variable
+  var html = '';
+
+  // build table headings
+  html += '<table cellpadding=10 cellspacing=0 border=1>';
+  html += '<tr>';
+  html += '<th>no</th>';
+  html += '<th>title</th>';
+  html += '<th>date</th>';
+  html += '</tr>';
+  
+  // loop to build html output for each row
+  var entry = data.feed.entry;
+  /**
+  ** Change to descending order
+  ** for (var i = entry.length - 1; i >= 0; i -= 1) {
+   */
+  for (var i = 0; i < entry.length; i++) {
+    html += '<tr>';
+    html += '<td>' + entry[i]['gsx$no']['$t'] + '</td>';
+    html += '<td>' + entry[i]['gsx$title']['$t'] + '</td>';
+    html += '<td>' + entry[i]['gsx$date']['$t'] + '</td>';
+    html += '</tr>';
+  }
+  html += '</table>';
+
+  // output html
+  $('.console').html(html);
 });
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
+
+// loading animation
+var loading = $('.loading');
+loading.hide();
+$(document)
+  .ajaxStart(function() {
+    loading.show();
+  })
+  .ajaxStop(function() {
+    loading.hide();
+  });
